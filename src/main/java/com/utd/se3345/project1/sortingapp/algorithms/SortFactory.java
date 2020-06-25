bbbf5970272d6bde9335855fd8e5b720221d953c
artin shouraki <mohsen.shoraki@gmail.com>
2020-06-25 14:54:44 -0500

    controller changes

--- src/main/java/com/utd/se3345/project1/sortingapp/algorithms/SortFactory.java
+++ src/main/java/com/utd/se3345/project1/sortingapp/algorithms/SortFactory.java
@@ -1,5 +1,14 @@
 package com.utd.se3345.project1.sortingapp.algorithms;
 
+import com.utd.se3345.project1.sortingapp.util.SortType;
+
 public class SortFactory {
+	
+	public Sort factory(String sortType) {
+		
+		switch(sortType) {
+		case SortType.   :return new InsertionSort();break;
+		}
+	}
 
 }
